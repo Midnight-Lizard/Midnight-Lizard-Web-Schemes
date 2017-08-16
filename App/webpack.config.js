965dd6c9789c6502151e127db6ce86af5ec947f2
Pavel Agarkov <pavel.agarkov@gmail.com>
2017-08-16 10:48:59 +0300

    AotPlugin

--- App/webpack.config.js
+++ App/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const AotPlugin = require('@ngtools/webpack').AotPlugin;
 const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
 
 module.exports = (env) =>
